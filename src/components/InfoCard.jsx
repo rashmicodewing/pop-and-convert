c1d19441c3bdf2fd3dc9183be1781b346a1d74e3
rashmi247 <rashmi.codewing@gmail.com>
2023-07-06 17:38:21 +0545

    added components

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,13 @@
+const InfoCard = () => {
+    return <div class="welcome-section">
+        <div class="title">Documentation</div>
+        <div className="content">
+            We're glad you're here. This platform is designed to
+            provide you with all the tools and resources you need to be a successful coach. 
+            Whether you're new to coaching or a seasoned pro, we're confident that you'll find everything you need here to help your athletes.
+        </div>
+        <buton href="#">Create New Notification</buton>
+    </div>
+}
+
+export default InfoCard
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar.jsx';
+import InfoCard from './components/InfoCard';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Navbar />
+    <InfoCard />
   </React.StrictMode>
 );
 
